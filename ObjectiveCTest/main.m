d6b09ee47cf5a639501e0aac23d15cd76e69f519
himanshu yadav <himanshuyadav@himanshus-MacBook-Pro.local>
2019-09-25 00:13:13 +0530

    Concepts Covered: Selectors, Some methods for working with dynamic types

--- ObjectiveCTest/main.m
+++ ObjectiveCTest/main.m
@@ -17,75 +17,88 @@
 Fraction * setHuman(Human * human, int age, int height, int weigth, int footLength);
 void distinctHumans(void);
 void print(id);
+void complexAndFractions(void);
+void humanAndDescendants(void);
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
-//        NSLog(@"sum is %i and sub is %i", sum, sub); //NSLog (Function to print) in %i i represents integer type of value
-        
-        //Two Fraction and two Human instances are created using alloc
-        Fraction *myFraction1 = [Fraction alloc];
-        Fraction *myFraction2 = [Fraction alloc];
-        myFraction1.numerator = 1;
-        myFraction1.denominator = 3;
-        
-        myFraction2.numerator = 1;
-        myFraction2.denominator = 4;
-        
-        Fraction *addedFraction = [myFraction1 add:myFraction2];
-//
-//        Human *firstHuman = [Human alloc];
-//        Human *secondHuman = [Human alloc];
-//
 //        distinctHumans();
-        
-        //Human properties are set
-//        myFraction1 = setHuman(firstHuman, 24, 170, 60, 10);
-//        myFraction2 = setHuman(secondHuman, 25, 180, 70, 10);
-        
-        //Human BMI is calculated using Fraction
-//        NSLog(@"BMI is %f", [myFraction1 getFraction]);
-//        NSLog(@"BMI is %f", [myFraction2 getFraction]);
-//        NSLog(@"Human count is %i", [Human getPopulation]);
-        Complex *complexNumber1 = [Complex alloc];
-        complexNumber1.real = 3;
-        complexNumber1.imaginary = 4;
-        
-        Complex *complexNumber2 = [Complex alloc];
-        complexNumber2.real = 4;
-        complexNumber2.imaginary = 5;
-        
-        Complex *addedComplexNumber = [complexNumber1 add:complexNumber2];
-        print(addedFraction);
-        print(addedComplexNumber);
-        
-return 0;
-}
+        complexAndFractions();
+    }
 }
 
 //Definition of the function
-Fraction * setHuman(Human * human, int age, int height, int weigth, int footLength){
-    [human setAge: age andHeight: height];
-    [human setOtherDetails :weigth :footLength];
-    Fraction *humanBMI = [Fraction alloc];
-    humanBMI.numerator = [human getBMINume];
-    humanBMI.denominator = [human getBMIDenom];
-    return humanBMI;
+Fraction *setHuman(Human *obj, int age, int height, int weigth, int footLength){
+    if([obj isKindOfClass:[Human class]]) {
+        NSLog(@"Human");
+        [obj setAge: age andHeight: height];
+        [obj setOtherDetails :weigth :footLength];
+        Fraction *humanBMI = [Fraction alloc];
+        humanBMI.numerator = [obj getBMINume];
+        humanBMI.denominator = [obj getBMIDenom];
+        return humanBMI;
+    }
+    return false;
 }
 
 void distinctHumans() {
+    Fraction *myFraction1 = [Fraction alloc];
+    Fraction *myFraction2 = [Fraction alloc];
     Male *male = [Male alloc];
     Female *female = [Female alloc];
     [male setMalePhyChar:40 :15: 17];
-    [male setAge:24 andHeight:180];
-    [male setOtherDetails:70 :9] ;
     [female setFemalePhyChar:36 :36: 24];
-    [female setAge:25 andHeight:165];
-    [female setOtherDetails:54 :7];
+    myFraction1 = setHuman(female, 24, 170, 60, 10);
+    myFraction2 = setHuman(male, 25, 180, 70, 10);
+    NSLog(@"BMI's of male and female are: %f %f", [myFraction1 getFraction], [myFraction2 getFraction]);
 }
 
 //id is used to refer to or store any type of Object
 void print(id obj) {
-    [obj print];
+    SEL printSelector = @selector(print);
+    [obj performSelector:printSelector];
+}
+
+void complexAndFractions() {
+    Fraction *myFraction1 = [Fraction alloc];
+    Fraction *myFraction2 = [Fraction alloc];
+    myFraction1.numerator = 1;
+    myFraction1.denominator = 3;
+    
+    myFraction2.numerator = 1;
+    myFraction2.denominator = 4;
+    
+    Fraction *addedFraction = [myFraction1 add:myFraction2];
+    
+    Complex *complexNumber1 = [Complex alloc];
+    complexNumber1.real = 3;
+    complexNumber1.imaginary = 4;
+    
+    Complex *complexNumber2 = [Complex alloc];
+    complexNumber2.real = 4;
+    complexNumber2.imaginary = 5;
+    
+    Complex *addedComplexNumber = [complexNumber1 add:complexNumber2];
+    
+    print(addedFraction);
+    print(addedComplexNumber);
+}
+
+void humanAndDescendants() {
+    //Two Fraction and two Human instances are created using alloc
+    Fraction *myFraction1 = [Fraction alloc];
+    Fraction *myFraction2 = [Fraction alloc];
+    Human *firstHuman = [Human alloc];
+    Human *secondHuman = [Human alloc];
+    distinctHumans();
+    
+    //Human properties are set
+    myFraction1 = setHuman(firstHuman, 24, 170, 60, 10);
+    myFraction2 = setHuman(secondHuman, 25, 180, 70, 10);
+    
+    //Human BMI is calculated using Fraction
+    NSLog(@"BMI is %f", [myFraction1 getFraction]);
+    NSLog(@"BMI is %f", [myFraction2 getFraction]);
+    NSLog(@"Human count is %i", [Human getPopulation]);
 }
 
